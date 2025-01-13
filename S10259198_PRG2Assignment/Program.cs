b8c6954574930b5676dbbf9345f9191db3ead958
Kai Kiat Xander Yeo <s10259198@connect.np.edu.sg>
2025-01-13 13:04:44 +0800

    Reading airlines.csv

--- S10259198_PRG2Assignment/Program.cs
+++ S10259198_PRG2Assignment/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using S10259198_PRG2Assignment;
+
+Terminal terminal = new Terminal("Terminal 5");
+Dictionary<string, Airline> Airlines = terminal.Airlines;
+
+using (StreamReader sr = new StreamReader("airlines.csv", true))
+{
+    string s;
+    string airlinename;
+    string airlinecode;
+    while ((s=sr.ReadLine()) != null)
+    {
+        string[] data = s.Split(',');
+
+        airlinename = data[0];
+        airlinecode = data[1];
+
+        Airline a = new Airline(data[0], data[1]);
+        terminal.Airlines.Add(airlinecode,a);
+    }
+}
+
+
+
+
+
+
+
+
+
+    Console.WriteLine("Loading Airlines...");
